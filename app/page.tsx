e1e5502c9f37c6aa0e379920898e6b29ef65f31b
nidzaaj <nidza.j.1992@gmail.com>
2025-02-06 02:59:43 +0100

    Tailwind CSS Setup and Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>DewFlow Application</h1>
+    <div className=" bg-dark-100 text-white p-4">
+      <h1 className="h1-bold">DewFlow Application</h1>
     </div>
   );
 }
